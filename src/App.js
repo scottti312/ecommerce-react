b6e14941e16d210ac13cc86c7f0df7180f0f64b3
Scott <scott.ti312@gmail.com>
2023-02-21 01:09:16 -0800

    Add addToCart logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Products from './pages/Products';
-import Cart from './pages/Cart';
 import About from './pages/About';
 import ProductPage from './pages/ProductPage';
 
 function App() {
+  const [cart, setCart] = useState(new Map());
+
+  function handleAddToCart(event, id, title, price) {
+    // Stops Link from calling an onClick to product page
+    // when the add to cart icon is clicked. At FeaturedStickers.js
+    event.preventDefault();
+    addToCart(id, title, price);
+  }
+
+  function addToCart(id, title, price) {
+    let product = {
+      id: id,
+      title: title,
+      price: price,
+    }
+    let productString = JSON.stringify(product);
+    if (cart.has(productString)) {
+      cart.set(productString, cart.get(productString) + 1);
+    } else {
+      cart.set(productString, 1);
+    }
+    setCart(cart); 
+  }
+
   return (
     <>
       <BrowserRouter>
-        <Navbar />
+        <Navbar cart={cart} key={cart.size}/>
         <Routes>
-          <Route path="/sticker-avenue" element={<Home />}/>
+          <Route path="/sticker-avenue" element={<Home addToCart={handleAddToCart}/>}/>
           <Route path="/products" element={<Products />} />
-          <Route path="/cart" element={<Cart />} />
           <Route path="/about" element={<About />} />
           <Route path="/products/:productId" element={<ProductPage />} />
         </Routes>
