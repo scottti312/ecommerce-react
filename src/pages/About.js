8cdb91e5b8e644b28e19921f00dc23e50ce5d6b0
Scott <scott.ti312@gmail.com>
2023-02-22 10:11:07 -0800

    Font add

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ import styled from 'styled-components/macro';
 const About = () => {
   return (
     <AboutWrapper>
-      <h1>About</h1>
+      <AboutTitle>About Us</AboutTitle>
       <p>Sticker Avenue is an online store that specializes in selling stickers of all kinds. Out inventory includes a wide variety of high-quality stickers that can be used to decorate laptops, water bottles, journals, and more. Sticker Avenue offers unique designs and themes with new designs frequently added to our collection. With fast and reliable shipping, Sticker Avenue is a great choice for anyone looking to add a little personality to their belongings with fun, colorful stickers. (Not a real store!!)</p>
   </AboutWrapper>
   );
@@ -12,6 +12,9 @@ const About = () => {
 
 const AboutWrapper = styled.div`
   padding: 100px;
+`;
+
+const AboutTitle = styled.h1`
 `;
 
 export default About;
