a57ac8d117640ded1f9a7f358d4d9a6ee6aad630
Scott <scott.ti312@gmail.com>
2023-02-24 10:02:35 -0800

    Remove unused import

--- src/components/FeaturedStickers.tsx
+++ src/components/FeaturedStickers.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components/macro';
 import { COLORS } from "../colors";
