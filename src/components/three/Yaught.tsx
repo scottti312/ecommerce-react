60c9d537f58f69b1440215662d37f06aff40e464
Scott <scott.ti312@gmail.com>
2023-03-04 13:41:26 -0800

    Move all three canvas operations to Yaught

--- src/components/three/Yaught.tsx
+++ src/components/three/Yaught.tsx
@@ -1,45 +1,77 @@
 import { useRef, useState } from 'react'
-import { useFrame, useLoader } from '@react-three/fiber'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { Mesh } from 'three';
+import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import styled from 'styled-components/macro';
+import { OrbitControls } from '@react-three/drei';
 
 interface YaughtProps {
   url: string;
 }
 
-function Yaught({url}: YaughtProps) {
+function Yaught({ url }: YaughtProps) {
   const gltf = useLoader(GLTFLoader, url);
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef<Mesh>(null)
   // Hold state for hovered and clicked events
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => {
-    if (ref.current) {
-      // ref.current.rotation.x += delta;
-    };
-  })
 
   const degToRad = (degrees: number) => (Math.PI / 180) * degrees;
 
+  function CameraHelper() {
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    return <group position={[0,0,1.5]}>
+      <cameraHelper args={[camera]} />
+    </group>
+  }
+
+  function LightHelper() {
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    return <group position={[3,3,8]}>
+      <cameraHelper args={[camera]} />
+    </group>
+  }
+
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
-    <mesh
-      rotation={[Math.PI / 2, 0, 0]}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-        <primitive
-          object={gltf.scene}
-          rotation={[degToRad(180), degToRad(90), degToRad(90)]}
-        />
-      {/* <icosahedronGeometry args={[1, 0]} /> */}
-      {/* <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} /> */}
-    </mesh>
+    <CanvasWrapper>
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <ambientLight intensity={0.3} />
+        <spotLight position={[4, 4, 8]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <mesh
+          rotation={[Math.PI / 2, 0, 0]}
+          ref={ref}
+          // scale={clicked ? 1.5 : 1}
+          onClick={(event) => click(!clicked)}
+          onPointerOver={(event) => hover(true)}
+          onPointerOut={(event) => hover(false)}>
+          <primitive
+            object={gltf.scene}
+            rotation={[degToRad(180), degToRad(90), degToRad(90)]}
+          />
+          {/* <icosahedronGeometry args={[1, 0]} /> */}
+          {/* <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} /> */}
+        </mesh>
+        {/* <CameraHelper /> */}
+        {/* <LightHelper /> */}
+        <OrbitControls />
+      </Canvas>
+
+    </CanvasWrapper>
   )
 }
+
+const CanvasWrapper = styled.div`
+  width: 400px;
+  height: 400px;
+  border-radius: 35px;
+  background-color: white;
+  box-shadow: inset 0 -3em 3em rgba(0, 0, 0, 0.1), 0 0 0 2px rgb(255, 255, 255),
+    0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
+`
+
 
 export default Yaught;
